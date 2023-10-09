babc5cd85f5fcbec7b5d2fc39559f121f94f616a
Alex Rocha <131891153+Axvand@users.noreply.github.com>
2023-10-09 11:37:38 -0300

    commit construtivo
    xommit de construção

--- mainJs/functionsAuxCart/encaminhamento.js
+++ mainJs/functionsAuxCart/encaminhamento.js
@@ -38,10 +38,10 @@ finalizar.addEventListener('click',()=>{
 
     //===========observaçao==========
 
-    const textObservacao = document.querySelector('.textObservacao').value;
+const textObservacao = document.querySelector('.textObservacao').value;
 
     //===========Formulario Endereço======
-   const Nome = document.querySelector('.Nome').value
+    const Nome = document.querySelector('.Nome').value
     const cep = document.querySelector('.cep').value
     const bairro = document.querySelector('.bairro').value
     const logradouro = document.querySelector('.logradouro').value
@@ -62,28 +62,6 @@ finalizar.addEventListener('click',()=>{
 
     }
 
-    console.log(`
-     Cliente: 
-     ${Nome}
-
-     Endereço Do Cliente: 
-     cep: ${cep};
-     Bairro: ${bairro}; 
-     Logradouro:${logradouro};
-     Localidade: ${localidade};
-     Lote : ${lote}
-      
-      /////////// 
-      Observação: 
-      ${textObservacao}
-      ///////// 
-      Pedidos Do Carrinho: 
-      ${resumoDosPedidos}
-      //////
-      Valor total:
-      ${carrinho.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}
-
-    `)
 
     const link = ` https://api.whatsapp.com/send?phone=5561996081625&text=Cliente:%20%20${Nome}%0A_____________________%0AEndere%C3%A7o%20Do%20Cliente:%20%0ACep:%20CEP;%0ABairro:%20${bairro};%20%0ALogradouro:%20${logradouro};%0ALocalidade:%20${localidade};%0ALote%20:%20${lote}%0A_____________________%0AValor%20total:%0A${carrinho.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}%0A_____________________%0AObserva%C3%A7%C3%A3o:%20%0A${textObservacao}%0A____________%20%0APedidos%20Do%20Carrinho:%20%0A${resumoDosPedidos}%0A`
 
